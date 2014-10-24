1001a513122f5bb55bea86e3fa928e29dbaed637
Awakening Byte <gchen@awakeningbyte.com>
2014-10-24 15:49:57 -0500

    complete index.html

---
+++ solution.R
@@ -0,0 +1,23 @@
+library("caret")
+set.seed(123)
+
+columns <- c("roll_belt","pitch_belt","yaw_belt","total_accel_belt","gyros_belt_x","gyros_belt_y","gyros_belt_z","accel_belt_x","accel_belt_y","accel_belt_z","magnet_belt_x","magnet_belt_y","gyros_arm_x","gyros_arm_y","gyros_arm_z","accel_arm_x","accel_arm_y","accel_arm_z","magnet_arm_x","magnet_arm_y","magnet_arm_z",
+"magnet_belt_z","roll_arm","pitch_arm","yaw_arm","total_accel_arm","roll_dumbbell","pitch_dumbbell","yaw_dumbbell","total_accel_dumbbell","gyros_dumbbell_x","gyros_dumbbell_y","gyros_dumbbell_z","accel_dumbbell_x","accel_dumbbell_y","accel_dumbbell_z","magnet_dumbbell_x","magnet_dumbbell_y","magnet_dumbbell_z","roll_forearm","pitch_forearm","yaw_forearm",
+"total_accel_forearm","gyros_forearm_x","gyros_forearm_y","gyros_forearm_z","accel_forearm_x","accel_forearm_y","accel_forearm_z","magnet_forearm_x","magnet_forearm_y","magnet_forearm_z","classe")
+
+data <- read.csv('pml-training.csv')[columns]
+inTraining <- createDataPartition(data$classe, p=0.6, list=FALSE)
+training <- data[inTraining,]
+testing <- data[-inTraining,]
+
+testSet <-read.csv('pml-testing.csv')[columns[-53]]
+
+ctrl <- trainControl(method="oob", number=10)
+rf <- train(classe ~., data=training, method="rf", preProcess="pca", trControl=ctrl)
+print(rf$finalModel)
+
+testing$classe2 <- predict(rf, newdata=testing)
+confusionMatrix(data=testing$classe2, testing$classe)
+
+
+testSet $predict <- predict(rf, newdata=testSet )
